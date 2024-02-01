1165c198ed9c0da60ef491a5f61e3deab588a4ba
Bill Kyriazopoulos <skabillium@gmail.com>
2024-02-01 16:11:28 +0200

    Scaffold basic retry function for async operations

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,35 @@
-export default function log() {
-    console.log('Hello from doover');
+type RetryOptions = {
+    retries?: number;
+    delay?: number;
+    minTimeout?: number;
+    maxTimeout?: number;
+};
+
+const wait = (milliseconds: number) =>
+    new Promise((resolve) => setTimeout(resolve, milliseconds));
+
+export default async function retry<T>(
+    operation: () => Promise<T>,
+    options?: RetryOptions,
+) {
+    const maxRetries = options?.retries ?? 3;
+    const delay = options?.delay ?? 0;
+    let retries = 0;
+
+    while (retries < maxRetries) {
+        try {
+            const result = await operation();
+            return result;
+        } catch (err) {
+            retries++;
+
+            if (retries < maxRetries) {
+                if (delay > 0) {
+                    await wait(delay);
+                }
+            }
+        }
+    }
+
+    throw new Error(`Failed after ${maxRetries} attempts`);
 }
