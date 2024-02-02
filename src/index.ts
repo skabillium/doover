c274afb4d48affe6512741d859e01f5cab0a9a93
Bill Kyriazopoulos <skabillium@gmail.com>
2024-02-02 15:58:35 +0200

    Add exponential backoffs with custom factor
    Also add better default handling for options

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,64 @@
 type RetryOptions = {
     retries?: number;
     delay?: number;
-    minTimeout?: number;
-    maxTimeout?: number;
+    factor?: number;
+    // minTimeout?: number;
+    // maxTimeout?: number;
 };
 
-const wait = (milliseconds: number) =>
+const waitFor = (milliseconds: number) =>
     new Promise((resolve) => setTimeout(resolve, milliseconds));
+
+function loadOptions(options?: RetryOptions) {
+    const defaults: RetryOptions = {
+        retries: 3,
+        delay: 0,
+        factor: 1,
+    };
+
+    if (!options) {
+        return defaults;
+    }
+
+    options.retries = options?.retries ?? 3;
+    options.delay = options?.delay ?? 0;
+    options.factor = options?.factor ?? 1;
+    // options.minTimeout = options?.minTimeout ?? null;
+    // options.maxTimeout = options?.maxTimeout ?? null;
+
+    return options;
+}
 
 export default async function retry<T>(
     operation: () => Promise<T>,
-    options?: RetryOptions,
+    opts?: RetryOptions,
 ) {
-    const maxRetries = options?.retries ?? 3;
-    const delay = options?.delay ?? 0;
+    const options = loadOptions(opts);
+
+    const maxRetries = options.retries;
+    let { delay } = options;
     let retries = 0;
+    let returnError: Error;
 
     while (retries < maxRetries) {
         try {
             const result = await operation();
             return result;
         } catch (err) {
+            returnError = err;
             retries++;
 
             if (retries < maxRetries) {
                 if (delay > 0) {
-                    await wait(delay);
+                    if (retries !== 1) {
+                        delay = delay * options.factor;
+                    }
+
+                    await waitFor(delay);
                 }
             }
         }
     }
 
-    throw new Error(`Failed after ${maxRetries} attempts`);
+    throw returnError;
 }
