2a81694a3f89459746b4c7441825ec3985fd0279
Bill Kyriazopoulos <skabillium@gmail.com>
2024-02-08 14:16:21 +0200

    Add basic test suit

--- test/full-suite.test.ts
+++ test/full-suite.test.ts
@@ -1,16 +1,50 @@
 import { describe, expect, it } from '@jest/globals';
-import retry from '../src/index';
 import test from 'node:test';
+import retry from '../src/index';
 
 describe('Full test suite', () => {
-    it('Should throw after retries', () => {
-        test('throw test', async () => {
-            try {
-                await retry(() => Promise.reject(new Error('Error')));
-            } catch (error) {
-                console.log(typeof error);
-                expect(error.message).toMatch('Failed');
+    it('Should return the correct value', () => {
+        test('return value', async () => {
+            async function getTwo() {
+                return 2;
             }
+
+            const two = await retry(() => getTwo());
+            expect(two).toBe(2);
+        });
+    });
+
+    it('Should retry', () => {
+        test('retries', async () => {
+            let attempts = 0;
+            try {
+                await retry(() => fetch('https://9djdj3939.com'), {
+                    onError() {
+                        attempts++;
+                    },
+                });
+            } catch (e) {}
+
+            // Attempts should be retries + initial attempt
+            expect(attempts).toBe(4);
+        });
+    });
+
+    it('Should bail', () => {
+        test('bail', async () => {
+            let attempts = 0;
+            try {
+                await retry(
+                    async (bail) => {
+                        bail();
+                    },
+                    {
+                        onError: () => attempts++,
+                    },
+                );
+            } catch (e) {}
+
+            expect(attempts).toBe(0);
         });
     });
 });
